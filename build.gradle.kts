41bf27d33b5121c5f399fb58fc7ef6d59c87ae58
jonm24853-lang <jonm24853@gmail.com>
2026-09-03 12:00:34 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,1 +1,20 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
+dependencyResolutionManagement {
+    repositoriesMode.set(
+        org.gradle.api.initialization.resolve.RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    )
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SalahModePro"
+include(":app")
